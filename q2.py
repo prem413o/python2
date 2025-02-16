e82435f9540a3a4f18095c25a9b896b89a997478
Prem Kumar <premkumar22316748@gmail.com>
2025-02-16 17:54:11 +0530

    new question

---
+++ q2.py
@@ -0,0 +1,8 @@
+l=int(input("Enter length of rectangle: "))
+b=int(input("Enter breadth of rectangle: "))
+
+area=l*b
+perimeter=2*(l+b)
+
+print("the area of rectangle is",area)
+print("the perimeter of rectangle is ",perimeter)
