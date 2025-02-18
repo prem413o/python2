959648a20ac16550ca74f519328597f94d43eeb6
Prem Kumar <premkumar22316748@gmail.com>
2025-02-18 22:21:18 +0530

    list question

---
+++ list.py
@@ -0,0 +1,11 @@
+movies=[]
+
+movie1=input("Enter your first number:")
+movie2=input("Enter your second number: ")
+movies3=input("Enter your third movie: ")
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movies3)
+
+print(movies)
